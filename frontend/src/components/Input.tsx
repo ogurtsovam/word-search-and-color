13309add3a837c7366a9e5ba8b31d85ce2968df6
ogurtsovam <ogurts.maria@gmail.com>
2025-11-03 19:32:31 +0200

    fix: Input value

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,10 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
 import { selectActiveInput, setActiveInput } from "../slices/activeInputSlice";
 
 function Input() {
   const dispatch = useDispatch();
   const activeInput = useSelector(selectActiveInput);
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    if (activeInput) {
+      setInput(activeInput)
+    } else {
+      setInput("")
+    }
+  }, [activeInput]);
 
   function setInputHtml(value: string) {
     dispatch(setActiveInput(value))
@@ -15,7 +24,7 @@ function Input() {
     <textarea
       className="form-control"
       rows={10}
-      value={activeInput}
+      value={input}
       onChange={(e) => setInputHtml(e.target.value)}
     />
   </>
